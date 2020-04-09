c8e907e2a637a5a8d5a46c574d2a26ed49e3f749
unknown <n.danielargueta@gmail.com>
2020-04-09 12:58:51 -0700

    third change

--- config/db.js
+++ config/db.js
@@ -3,15 +3,7 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = () => {
-    mongoose.connect(db, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }).then(() => console.log('MongoDB connected'))
-    .catch(err => {
-        console.error(err.message);
-        process.exit(1);
-    });
+    mongoose.connect(db)
 };
 
 module.exports = connectDB;
