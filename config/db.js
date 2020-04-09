d5c05c8d5f30d9717f4c116ec437aa9fad2af1ae
unknown <n.danielargueta@gmail.com>
2020-04-09 13:39:05 -0700

    fourth commit

--- config/db.js
+++ config/db.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-    mongoose.connect(db)
-};
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+    } catch (error) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
 
 module.exports = connectDB;
